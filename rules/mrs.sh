d318ea004374929f3bf834820712599997b72027
David Xiang <admin@mdavid.cn>
2025-12-19 04:22:00 +0800

    upt

--- rules/mrs.sh
+++ rules/mrs.sh
@@ -5,3 +5,5 @@ mihomo="/Applications/Clash Verge.app/Contents/MacOS/verge-mihomo"
 curl -sSL https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-for-clash.yaml -o anti-ad-white.yaml
 "${mihomo}" convert-ruleset domain yaml anti-ad-white.yaml anti-ad-white.mrs
 rm anti-ad-white.yaml
+
+"${mihomo}" convert-ruleset ipcidr yaml dns.yaml dns.mrs
