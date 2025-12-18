df10ea24b2bf534cc4fcbdcbc05a5a97e8ef54fd
David Xiang <admin@mdavid.cn>
2025-12-19 04:33:58 +0800

    upt

--- rules/mrs.sh
+++ rules/mrs.sh
@@ -8,3 +8,4 @@ rm anti-ad-white.yaml
 
 "${mihomo}" convert-ruleset ipcidr yaml dns.yaml ./mrs/dns.mrs
 "${mihomo}" convert-ruleset domain yaml direct-domain.yaml ./mrs/direct-domain.mrs
+"${mihomo}" convert-ruleset domain yaml proxy-domain.yaml ./mrs/proxy-domain.mrs
