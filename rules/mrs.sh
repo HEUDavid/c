a70b755bac25e7f094b6d68880a956d31f9506fc
David Xiang <admin@mdavid.cn>
2025-12-19 15:48:25 +0800

    upt

--- rules/mrs.sh
+++ rules/mrs.sh
@@ -6,8 +6,12 @@ curl -sSL https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/
 "${mihomo}" convert-ruleset domain yaml anti-ad-white.yaml ./mrs/anti-ad-white.mrs
 rm anti-ad-white.yaml
 
-"${mihomo}" convert-ruleset ipcidr yaml dns.yaml ./mrs/dns.mrs
-"${mihomo}" convert-ruleset domain yaml direct-domain.yaml ./mrs/direct-domain.mrs
-"${mihomo}" convert-ruleset domain yaml proxy-domain.yaml ./mrs/proxy-domain.mrs
-"${mihomo}" convert-ruleset domain yaml proxy-white.yaml ./mrs/proxy-white.mrs
-"${mihomo}" convert-ruleset domain yaml reject-domain.yaml ./mrs/reject-domain.mrs
+for file in *domain*.yaml; do
+  name=$(basename "$file" .yaml)
+  "${mihomo}" convert-ruleset domain yaml "./$file" "./mrs/${name}.mrs"
+done
+
+for file in *ipcidr*.yaml; do
+  name=$(basename "$file" .yaml)
+  "${mihomo}" convert-ruleset ipcidr yaml "./$file" "./mrs/${name}.mrs"
+done
